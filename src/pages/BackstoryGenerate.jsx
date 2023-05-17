42d8967fe77e9387ad54b4893778fc480026d4c6
Gerrit Brinsat <gerrit.brinsat@gmx.de>
2023-05-17 13:50:24 +0200

    testing

--- src/pages/BackstoryGenerate.jsx
+++ src/pages/BackstoryGenerate.jsx
@@ -120,7 +120,6 @@ function BackstoryGenerate() {
         let createdBackstory = null
 
         const storedToken = localStorage.getItem("authToken")
-        console.log(/\t\r\n/.test(storedToken))
         const body = {option1, option2, option3, character: characterId}
 
         if(!userData.character[userData.character.length-1].backstory[0]) {
